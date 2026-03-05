8b5fdab964c1364370b2e5f25254a7c26dc1f7b7
NekoKaicho <adecta7@gmail.com>
2026-03-05 11:03:25 +0700

    Menambahakan fungsi input dengan maksimal 20 elemen

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -4,3 +4,15 @@ using namespace std;
 
 int arr[20];  //Deklarasi variabel global array a dengan ukuran 20 
 int n:;  //Deklarasi variabel global n untuk menyimpan banyaknya elemen
+
+void input() {  // Prosedur untuk input
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array :"; // Output ke layar
+        cin >> n; // input dari pengguna
+        if (n <= 20)  // Jika n kurang atau sama dengan 20
+            break; // Keluar dari loop
+        else { // Jika n lebih dari 20}
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
+    }
+}
